ffccf672e9c5e1d575e3c1233ed00ae6dd60012d
JulaoBR <julioscp2009@hotmail.com>
2018-01-28 23:31:29 -0200

    TRABALHO DE REQUISITOS

--- ProEstoque/ProEstoque/frmMenu.cs
+++ ProEstoque/ProEstoque/frmMenu.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace ProEstoque
 {
-    public partial class Form1 : Form
+    public partial class frmMenu : Form
     {
-        public Form1()
+        public frmMenu()
         {
             InitializeComponent();
         }
+
+       
     }
 }
